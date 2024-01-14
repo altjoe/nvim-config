046b8eca60cd883248479e802c86aa211864391e
altjoe <meyer.joer5@gmail.com>
2024-01-13 22:39:32 -0500

    added ability to close windows with space-w

--- lua/altjoe/core/remap.lua
+++ lua/altjoe/core/remap.lua
@@ -6,4 +6,4 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pt", ":Neotree toggle<CR>") 
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>pr', ':vsplit | wincmd L | terminal python3 %<CR>', {noremap = true, silent = true})
-
+vim.api.nvim_set_keymap ('n', '<leader>w', ':q<CR>', { noremap = true, silent = true })
