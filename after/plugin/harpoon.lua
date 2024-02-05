6dec6e46ffda047d96dfd1c4513a8eb78a51f68a
altjoe <meyer.joer5@gmail.com>
2024-02-05 11:19:42 -0500

    more options to harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -22,8 +22,10 @@ vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end, { noremap 
 vim.keymap.set("n", "<C-p>", function() harpoon:list():select(2) end, { noremap = true, silent = true })
 vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end, { noremap = true, silent = true })
 vim.keymap.set("n", "<C-m>", function() harpoon:list():select(4) end, { noremap = true, silent = true })
-
-
+vim.keymap.set("n", "<C-M-u>", function() harpoon:list():select(5) end, { noremap = true, silent = true })
+vim.keymap.set("n", "<C-M-p>", function() harpoon:list():select(6) end, { noremap = true, silent = true })
+vim.keymap.set("n", "<C-M-n>", function() harpoon:list():select(7) end, { noremap = true, silent = true })
+vim.keymap.set("n", "<C-M-m>", function() harpoon:list():select(8) end, { noremap = true, silent = true })
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
