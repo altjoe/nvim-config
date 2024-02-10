6ee23cddf3467c80bd4172baf0d65d83fefa6378
altjoe <meyer.joer5@gmail.com>
2024-02-10 15:13:08 -0500

    Adding an init file to plugins, I believe git being imported after neotree was causing working directory issues

---
+++ lua/altjoe/plugins/init.lua
@@ -0,0 +1,17 @@
+require("altjoe.plugins.colorscheme")
+require("altjoe.plugins.comment-toggle")
+require("altjoe.plugins.copilot")
+require("altjoe.plugins.harpoon")
+require("altjoe.plugins.lsp-zero")
+require("altjoe.plugins.neogit")
+-- telescope
+require("altjoe.plugins.neo-tree")
+require("altjoe.plugins.telescope")
+-- treesitter
+require("altjoe.plugins.treesitter")
+-- undotree
+require("altjoe.plugins.undotree")
+-- vim-be-good
+require("altjoe.plugins.vim-be-good")
+require("neo-tree")
+--
