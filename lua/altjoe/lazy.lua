6d790a380c7251fba9963c39f215d97e1197461f
altjoe <meyer.joer5@gmail.com>
2024-02-20 20:09:36 -0500

    Fixing svelte treesitter not highlighting properly

--- lua/altjoe/lazy.lua
+++ lua/altjoe/lazy.lua
@@ -4,15 +4,22 @@ print("Loading lazy config...")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("altjoe.plugins")
+local ok, treesitter = pcall(require, "nvim-treesitter.configs")
+if ok then
+    treesitter.setup({
+        ensure_installed = { "typescript", "css", "javascript", "svelte" },
+        highlight = { enable = true },
+    })
+end
