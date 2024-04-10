f4a09d5fdc02ec3923ab6eb4124efd21adb235ea
altjoe <meyer.joer5@gmail.com>
2024-04-09 20:24:17 -0400

    true copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,9 +1,17 @@
 -- restart copilot service
 vim.keymap.set("n", "<leader>co", function()
 	-- Copilot !toggle
+	-- true if not initialized
+	if vim.g.copilot_enabled == nil then
+		vim.g.copilot_enabled = true
+	else
+		vim.g.copilot_enabled = not vim.g.copilot_enabled
+	end
 
-	vim.api.nvim_command("Copilot! detach")
-	vim.api.nvim_command("Copilot! attach")
-
+	if vim.g.copilot_enabled then
+		vim.api.nvim_command("Copilot! attach")
+	else
+		vim.api.nvim_command("Copilot! detach")
+	end
 	vim.api.nvim_command("Copilot status")
 end)
